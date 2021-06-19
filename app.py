f87821a4499f7c1fb98126971c73c9fc50747de1
lemocla <lemocla@icloud.com>
2021-06-19 02:41:48 +0000

    Adjust layout flag page

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import json
 from flask import Flask, render_template, request, redirect, url_for, flash, session
 
 app = Flask(__name__)
@@ -11,7 +12,10 @@ def index():
 
 @app.route("/flags")
 def flags():
-    return render_template("flags.html")
+    data = []
+    with open("data/flags.json", "r") as json_data:
+        data = json.load(json_data)
+    return render_template("flags.html", page_title="Meet the flags", flags=data)
 
 
 if __name__ == "__main__":
