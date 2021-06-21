27a6fb951925bbac58b4d66f6316390db141425c
Anthony O'Brien <aux.fuse@yahoo.com>
2021-06-21 10:00:17 +0100

    app.py file restructured with test 500 view function to view file

--- app.py
+++ app.py
@@ -10,32 +10,9 @@ def index():
     return render_template('index.html')
 
 
-@app.route("/accessibility")
-def accessibility():
-    return render_template('accessibility.html')
-
-
 @app.route("/home")
 def home():
     return render_template('home.html', page_title="Over the Rainbow")
-
-
-@app.route("/error500")
-def error500():
-    return render_template('error-500.html')
-
-
-@app.route("/flags")
-def flags():
-    data = []
-    with open("data/flags.json", "r", encoding="utf8") as json_data:
-        data = json.load(json_data)
-    return render_template("flags.html", page_title="Meet the flags", flags=data)
-
-
-@app.route("/privacy")
-def privacy():
-    return render_template('privacy-policy.html')
 
 
 @app.route("/support")
@@ -46,9 +23,40 @@ def support():
     return render_template("support.html", page_title="Supporting LGBTQ+ Community", advocates=data)
 
 
-@app.route("/error")
-def error():
-    return render_template("404.html")
+@app.route("/flags")
+def flags():
+    data = []
+    with open("data/flags.json", "r", encoding="utf8") as json_data:
+        data = json.load(json_data)
+    return render_template("flags.html", page_title="Meet the flags", flags=data)
+
+
+@app.route("/accessibility")
+def accessibility():
+    return render_template('accessibility.html')
+
+
+@app.route("/privacy")
+def privacy():
+    return render_template('privacy-policy.html')
+
+
+@app.route("/e500")
+def e500():
+    return render_template('500.html')
+
+
+# Error Handling
+@app.errorhandler(404)
+def response_404(exception):
+    """When 404 is captured display custom 404.html page"""
+    return render_template('404.html', exception=exception)
+
+
+@app.errorhandler(500)
+def response_500(exception):
+    """When 500 is captured display custom 500.html page"""
+    return render_template('500.html', exception=exception)
 
 
 if __name__ == "__main__":
